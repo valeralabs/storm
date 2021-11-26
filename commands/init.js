049ec0786c0927a7e79ea6e30ca43e5d56819eed
asteria <asteria@syvita.org>
2021-11-26 22:06:42 +0000

    fix create new key during init

--- commands/init.js
+++ commands/init.js
@@ -1,77 +1,102 @@
-import animation from "chalk-animation";
-import ora from 'ora';
-import chalk from "chalk";
-import prompts from "prompts";
-import bip39 from "bip39";
-import { createSecretKey, getSecretKey, importSecretKey } from "../lib/vault";
-const log = console.log;
+import animation from 'chalk-animation'
+import ora from 'ora'
+import chalk from 'chalk'
+import prompts from 'prompts'
+import bip39 from 'bip39'
+import { createSecretKey, getSecretKey, importSecretKey } from '../lib/vault'
+const log = console.log
 
 export default async function init() {
   const key = await getSecretKey()
-  
+
   if (key) {
-    log(chalk.red('\nStorm is already setup. If you want to change the key, run `storm reset`\n'));
-    return process.exit(0);
+    log(
+      chalk.red(
+        '\nStorm is already setup. If you want to change the key, run `storm reset`\n'
+      )
+    )
+    return process.exit(0)
   }
 
-  let anim = animation.glitch(`\nS T O R M\n`);
-  await new Promise(res => setTimeout(res, 1500));
-  anim.stop();
-  log("Hey there!");
-  log("Welcome to the Storm CLI!");
+  let anim = animation.glitch(`\nS T O R M\n`)
+  await new Promise(res => setTimeout(res, 1500))
+  anim.stop()
+  log('Hey there!')
+  log('Welcome to the Storm CLI!')
 
-  log(
-      "Storm is a secure authenticator and wallet available from your terminal"
-  );
+  log('Storm is a secure authenticator and wallet available from your terminal')
 
   const response = await prompts({
-    type: "confirm",
-    name: "value",
-    message: "Ready?",
-    initial: true
-  });
+    type: 'confirm',
+    name: 'value',
+    message: 'Ready?',
+    initial: true,
+  })
 
   if (response.value) {
     log('\nAwesome. To get started, we need to create or import a Secret Key.')
-    log("The Secret Key is stored securely in your operating system's keychain.")
+    log(
+      "The Secret Key is stored securely in your operating system's keychain."
+    )
 
     const response = await prompts({
       type: 'select',
       name: 'shouldCreateKey',
       message: 'Key retrieval',
       choices: [
-        { title: 'Create a Key', description: "We'll create a new random Key for you", value: true },
-        { title: 'Import a Key', description: "Use a Key you already have", value: false }
+        {
+          title: 'Create a Key',
+          description: "We'll create a new random Key for you",
+          value: true,
+        },
+        {
+          title: 'Import a Key',
+          description: 'Use a Key you already have',
+          value: false,
+        },
       ],
-      initial: 1
+      initial: 1,
     })
 
-    if (response.shouldCreateKey){
-      const spinner = ora('Creating Secret Key').start();
-      const key = await createSecretKey();
-      spinner.succeed('Created Secret Key');
-      log(chalk.green('\nSetup successful! Storm is ready.'));
-      log(chalk.bgYellow.black(' ⚠ Ensure you have backed up your key! \n'));
+    if (response.shouldCreateKey) {
+      const { password } = await prompts({
+        type: 'password',
+        name: 'password',
+        message: 'Enter a password',
+        validate: password =>
+          password.length < 6 ? 'Password must be at least 6 characters' : true,
+      })
+
+      const spinner = ora('Creating Secret Key').start()
+      const key = await createSecretKey(128, password)
+      spinner.succeed(`Created Secret Key: ${key}`)
+      log(chalk.green('\nSetup successful! Storm is ready.'))
+      log(chalk.bgYellow.black(' ⚠ Ensure you have backed up your key! \n'))
     } else {
       const { key, password } = await prompts([
         {
           type: 'text',
           name: 'key',
           message: 'Enter your Secret Key',
-          validate: key => !bip39.validateMnemonic(key) ? 'Invalid Secret Key' : true
+          validate: key =>
+            !bip39.validateMnemonic(key) ? 'Invalid Secret Key' : true,
         },
         {
           type: 'password',
           name: 'password',
           message: 'Enter a password',
-          validate: password => password.length < 6 ? 'Password must be at least 6 characters' : true
-        }
+          validate: password =>
+            password.length < 6
+              ? 'Password must be at least 6 characters'
+              : true,
+        },
       ])
-      log();
-      const spinner = ora('Importing Secret Key').start();
-      await importSecretKey(key, password);
-      spinner.succeed('Imported Secret Key');
-      log(chalk.green('\nSetup successful! Storm is ready.'));
-      log(chalk.bgYellow.black(' ⚠ Ensure you have backed up your key! \n'));
-  }}
+      log()
+      const spinner = ora('Importing Secret Key').start()
+      await importSecretKey(key, password)
+      spinner.succeed('Imported Secret Key')
+      log(chalk.green('\nSetup successful! Storm is ready.'))
+      log(chalk.bgYellow.black(' ⚠ Ensure you have backed up your key! \n'))
+    }
+  }
 }
