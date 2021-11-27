4743a231e2a1a41d1ce51b4223971e615ecb8fad
asteria <asteria@syvita.org>
2021-11-27 14:11:47 +0000

    switch accounts to use chooseAccount

--- commands/accounts.js
+++ commands/accounts.js
@@ -1,37 +1,10 @@
 import { pbcopy } from '../lib/copy'
 import chalk from 'chalk'
-import ora from 'ora'
-import prompts from 'prompts'
-import { getAccountChoices } from '../lib/accounts'
-import { autosearch } from '../lib/autosearch'
-import { promptPassword } from '../lib/prompt'
-import { getWallet } from '../lib/vault'
+import { chooseAccount } from '../lib/accounts'
 
 export async function getAccounts() {
-  const password = await promptPassword()
-
-  const spinner = ora('Loading wallet').start()
-
-  const wallet = await getWallet(password).then(wallet => {
-    spinner.text = 'Loading BNS names'
-    return wallet
-  })
-
-  const accounts = await getAccountChoices(wallet).then(accounts => {
-    spinner.succeed('Wallet loaded')
-    return accounts
-  })
-
-  const { address } = await prompts({
-    type: 'autocomplete',
-    name: 'address',
-    message: 'Choose an account',
-    choices: accounts,
-    suggest: async (input, choices) => await autosearch(input, choices),
-  }).catch(err => {
-    console.log(chalk.red(err.message))
-    process.exit(1)
-  })
+  const account = await chooseAccount()
+  const address = account.address
 
   const copied = pbcopy(address)
   if (copied) {
