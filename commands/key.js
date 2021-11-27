5d7ac8ca9f0e69d6dbe304f7e90c5054f35f2b80
asteria <asteria@syvita.org>
2021-11-27 09:07:32 +0000

    change key.js to use promptPassword

--- commands/key.js
+++ commands/key.js
@@ -1,12 +1,7 @@
-import chalk from 'chalk'
-import prompts from 'prompts'
+import { promptPassword } from '../lib/prompt'
 import { getSecretKey } from '../lib/vault'
 
 export default async function key() {
-  const { password } = await prompts({
-    type: 'password',
-    name: 'password',
-    message: 'Enter your password:',
-  })
+  const password = await promptPassword()
   console.log(await getSecretKey(password))
 }
